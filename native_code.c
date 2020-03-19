e5bf28298ca93bc001e0cf76fa92ac28ee1344d8
Heejun Kim <heejun.kim@linecorp.com>
2020-03-19 14:40:07 +0900

    add native test code

---
+++ native_code.c
@@ -0,0 +1,16 @@
+/* File : native_code.c */
+
+#include "native_code.h"
+
+/* Compute factorial of n */
+int fact(int n) {
+  if (n <= 1)
+    return 1;
+  else
+    return n*fact(n-1);
+}
+
+/* Compute n mod m */
+int my_mod(int n, int m) {
+  return(n % m);
+}
---
+++ native_code.h
@@ -0,0 +1,7 @@
+
+/* File : native_code.h */
+
+double  My_variable  = 3.0;
+
+int fact(int n);
+int my_mod(int n, int m);
